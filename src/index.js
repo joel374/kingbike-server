2e46dd8b9b59fa719a223a9d53e16ca0031fcc17
Joel <joellegifanimanisali@gmail.com>
2026-05-02 10:16:05 +0700

    feat: initialize express server with application routes and database synchronization

--- src/index.js
+++ src/index.js
@@ -38,6 +38,18 @@ app.use("/transaction", transactionRoute);
 app.use("/warehouse", warehouseRoute);
 app.use("/rajaongkir", rajaOngkirRoute);
 app.use("/carousel", carouselRoute);
+app.use("/greetings", (req, res) => {
+  try {
+    return res.status(200).json({
+      message: "Greetings",
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "Server Error",
+    })
+  }
+});
 
 app.listen(PORT, (err) => {
   if (err) {
